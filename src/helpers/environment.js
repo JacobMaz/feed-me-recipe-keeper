8af16bc75efc51f9a58e31eae8539b13b06b8570
JakeMaz <jakemazanowski@gmail.com>
2021-01-12 11:43:05 -0500

    added heroku case

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -2,7 +2,10 @@ let APIURL = '';
 
 switch(window.location.hostname){
     case 'localhost' || '127.0.0.1':
-        APIURL = 'http://localhost:3210'
+        APIURL = 'http://localhost:3210';
+        break;
+    case 'feed-me-recipe-keeper.herokuapp.com':
+        APIURL = 'https://feed-me-recipe-keeper.herokuapp.com/'
 }
 
 export default APIURL
